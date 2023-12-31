3ebbda96a73f61f8977c8140e64afccbb146bbbb
yongjie.chen <58081580+CBCZJL@users.noreply.github.com>
2023-12-31 13:11:14 +0800

    Add files via upload

---
+++ detection/train_det.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+CONFIG=$1
+GPUS=$2
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
+PORT=${PORT:-29500}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch \
+    --nnodes=$NNODES \
+    --node_rank=$NODE_RANK \
+    --master_addr=$MASTER_ADDR \
+    --nproc_per_node=$GPUS \
+    --master_port=$PORT \
+    $(dirname "$0")/train.py \
+    $CONFIG \
+    --seed 0 \
+    --launcher pytorch ${@:3}
+    
+# CUDA_VISIBLE_DEVICES=0,1,2,3 bash detection/train_det.sh detection/configs/yj_pvt_t.py 4
