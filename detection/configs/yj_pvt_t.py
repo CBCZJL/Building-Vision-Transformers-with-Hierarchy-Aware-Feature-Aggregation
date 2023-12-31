a15b6ea1a6258f51df10b9b05142720e3f384e39
yongjie.chen <58081580+CBCZJL@users.noreply.github.com>
2023-12-31 13:09:48 +0800

    Add files via upload

---
+++ detection/configs/yj_pvt_t.py
@@ -0,0 +1,31 @@
+_base_ = [
+    '../configs/_base_/models/mask_rcnn_r50_fpn.py',
+    '../configs/_base_/datasets/coco_instance.py',
+    '../configs/_base_/schedules/schedule_1x.py',
+    '../configs/_base_/default_runtime.py'
+]
+model = dict(
+    # pretrained='pretrained/pvt_tiny.pth',
+    pretrained='',
+    backbone=dict(
+        type='pvt_tiny',
+        style='pytorch'),
+    # neck=dict(
+    #     type='FPN',
+    #     in_channels=[64, 128, 320, 512],
+    #     out_channels=256,
+    #     num_outs=5)
+    neck=dict(
+        type='MTA',
+        in_channels=[64, 128, 320, 512],
+        out_channels=256,
+        start_level=0,
+        # add_extra_convs='on_input',
+        num_heads=[4, 4, 4, 4],
+        mlp_ratios=[4, 4, 4, 4],
+        num_outs=5,
+        use_sr_layer=False,
+    ))
+# optimizer
+optimizer = dict(_delete_=True, type='AdamW', lr=0.0001, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
